4500bf42fd52c8ef0c3127fdd69485fbbb30f445
WarsayT <warsay.teklebrhan@gmail.com>
2022-11-01 23:21:36 +0400

    first upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
+/* eslint-disable no-unused-vars */
 import logo from './logo.svg';
 import './App.css';
+import { Link, Outlet } from 'react-router-dom';
 
-function App() {
+export function Navigation() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/about/history">History</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+  )
 }
 
-export default App;
+function Home() {
+  return (
+    <div>
+      <h1>My Website</h1>
+    </div>
+  )
+}
+
+export function About() {
+  return(
+    <div>
+      <h1>About us</h1>
+      <p>This is about us page</p>
+      <Outlet />
+    </div>
+  )
+}
+
+export function History() {
+  return (
+    <div>
+      <h1>Our History</h1>
+      <p>Lorem ipsum is a strick lorem ipsum 1998.</p>
+    </div>
+  )
+}
+
+export function Contact() {
+  return (
+    <div>
+      <h1>Contact us</h1>
+    </div>
+  )
+}
+
+export function App() {
+
+  return (
+      <Home />
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,49 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { 
+  Navigation, App, About, Contact, History 
+} from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Navigation />
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/about' element={<About />}>
+        <Route path='history' element={<History />} />
+      </Route>
+      <Route path='/contact' element={<Contact />} />
+    </Routes>
+  </BrowserRouter>
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
